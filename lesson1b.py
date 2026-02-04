a40f82b959188dc16fbfc96c1cc5fb1921be6247
Vkkkyyy <githinjivictoria06@gmail.com>
2026-02-04 13:23:10 +0300

    add python

---
+++ lesson1b.py
@@ -0,0 +1,16 @@
+# Python Data types
+# String- It's a series of series of characters that are inside of quotes
+name="Joseph Mbugua"
+print(name)
+print(type(name))
+
+#Numeric
+#Int-This indicate a whole number in python.For an integer , we don't use quotes
+age= 39
+print("The age of a person is:", age)
+print(type(age))
+
+# float- this is a number with a decimal
+coordinates= 15.4524
+print("The coordinate are: ", coordinates)
+print(type(coordinates))
