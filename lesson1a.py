29b369e5b05028414eab0c1b557a8f1bfe3723f2
Vkkkyyy <githinjivictoria06@gmail.com>
2026-02-04 13:08:17 +0300

    added python

---
+++ lesson1a.py
@@ -0,0 +1,22 @@
+#What are variables: Variable are labels which as containers to hold values.  In python, a variable is always associated with some values.
+# We use the assignment operator(=) to assign values to a variable.
+#Anything that is inside of quotes in python is called a string
+message= 'Hello there! Hope everything is fine?'
+print(message)
+
+message2="  Good Bye!"
+print(message2)
+
+#concatenating variable (Joining two or more outputs together)
+message3= message + message2
+print(message3)
+
+#variable can be named using a letter, number and an underscore. They can start with a letter and an underscore but not with a number
+_name1="Brenda"
+print(_name1)
+
+#Variable names can not contain spaces . We can use the underscore to separate the names.
+total_marks=85
+print(total_marks)
+
+#variable names are not supposed to e keywords/ reserved characters in python
