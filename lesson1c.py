46af1bfd4c93a5263d17f6a780916df42d4d61ea
Vkkkyyy <githinjivictoria06@gmail.com>
2026-02-04 14:13:45 +0300

    Python

---
+++ lesson1c.py
@@ -0,0 +1,41 @@
+# Arithmetic Operations
+#Additon 
+number1= 10
+number2= 20
+addition= number1 + number2
+print("The sum of the two numbers is: ", addition)
+
+#subtraction
+number3= 50
+number4= 15
+difference= number3 - number4
+print("The differnce of the number is :", difference) 
+
+#multiplication
+number5=10
+number6=6
+product= number5 * number6
+print("The product of numbers is:", product)
+
+#division
+number7= 1000
+number8=50
+division= number7 / number8
+print("The division of the numbers is: ", division)
+
+#modulus-This is means the remainder of a number when get subdivided by another another one
+number9=14
+number10=5 
+modulus= number9 % number10
+print("The modulus of the number is:", modulus)
+
+
+#Floor division - This is the number of times a number subdivides another ignoring the number
+floor=number9 // number10
+print("The floor division of the two number is:", floor)
+
+#Exponent(**)
+#The power of a number
+number11=10
+exponent=number11 ** 2
+print("The exponent of the number is: ", exponent )
---
+++ lesson1d.py
@@ -0,0 +1,15 @@
+#Create a python that is able to calculate the simple intest given the principle as 50000, rate as 5% as 8 years .SI= (p*r*t)/100
+principle= 50000
+rate = 5
+time = 8
+Simpleinterest = (principle * rate * time)/ 100
+print("The simle interest is: ", Simpleinterest)
+
+
+#Assignment
+#1.Write a python that is able to calculate the BMI of a person whose weight is 78 kgs and height is 1.75 meters.BMI = (weight)/(height squared)
+
+
+# 2. Find the Area and perimeter of a rectangle whose length is 48cm and width is 25cm
+
+# Research on python list, tuple and Dictionary Data types.
