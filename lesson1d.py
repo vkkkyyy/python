5d363288eee5afd1b133868778d294893fc5ebaa
Vkkkyyy <githinjivictoria06@gmail.com>
2026-02-05 11:32:47 +0300

    python

--- lesson1d.py
+++ lesson1d.py
@@ -8,8 +8,17 @@ print("The simle interest is: ", Simpleinterest)
 
 #Assignment
 #1.Write a python that is able to calculate the BMI of a person whose weight is 78 kgs and height is 1.75 meters.BMI = (weight)/(height squared)
-
-
+weight = 78
+height = 1.75
+BMI =(weight)/ (height **2)
+print("This is the BMI", BMI)
 # 2. Find the Area and perimeter of a rectangle whose length is 48cm and width is 25cm
-
+length = 48
+width = 25
+area = length * width
+perimeter = (length + width)* 2
+print("this is the area", area)
+print("this is the perimeter", perimeter)
 # Research on python list, tuple and Dictionary Data types.
+thislist = ["apple" , "banana" ,"cherry" ,"apple", "cherry"]
+print(thislist)
